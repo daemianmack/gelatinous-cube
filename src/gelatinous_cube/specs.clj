7c594e7a5583bd9fd5bfb7b332256a1d43e1329a
Daemian Mack <daemianmack@gmail.com>
2022-09-14 15:51:57 -0400

    Expand `::specs/norm-maps` to consider vector inline data sources

--- src/gelatinous_cube/specs.clj
+++ src/gelatinous_cube/specs.clj
@@ -2,8 +2,15 @@
   (:require [clojure.spec.alpha :as s]
             [gelatinous-cube.tx-sources :as tx-sources]))
 
+(s/def ::op #{:db/add :db/retract})
 
-(s/def ::tx-data (s/coll-of map?))
+(s/def ::vector-form (s/cat :op ::op
+                            :n-identifier some? ;; More granular?
+                            :identifier keyword?
+                            :value some?))
+
+(s/def ::tx-data-form (s/nonconforming (s/or :map map? :vector ::vector-form)))
+(s/def ::tx-data (s/coll-of ::tx-data-form))
 (s/def ::tx-fn symbol?)
 (s/def ::tx-resource string?)
 
